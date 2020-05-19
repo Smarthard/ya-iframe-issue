a459c86a43f37ea9d0a478cae7bdb5de5b8e19c3
Smarthard <th3smartchan@gmail.com>
2020-05-19 16:29:59 +0300

    fix quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
