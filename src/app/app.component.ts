812eff2bb44d962443dd36b4f6626f3f01f80e38
Smarthard <th3smartchan@gmail.com>
2020-05-19 16:45:00 +0300

    input event timeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {fromEvent} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   readonly DEFAULT_URL = 'https://video.sibnet.ru/shell.php?videoid=2984574';
+
+  @ViewChild('urlInput', { static: true })
+  inputField: ElementRef<HTMLInputElement>;
 
   url = this.DEFAULT_URL;
 
   constructor(private _sanitizer: DomSanitizer) {}
+
+  ngAfterViewInit(): void {
+    fromEvent(this.inputField.nativeElement, 'input')
+      .pipe(debounceTime(500))
+      .subscribe((evt) => this.url = evt.target.value);
+  }
 
   sanitizeUrl(url: string): SafeUrl {
     return this._sanitizer.bypassSecurityTrustResourceUrl(url);
